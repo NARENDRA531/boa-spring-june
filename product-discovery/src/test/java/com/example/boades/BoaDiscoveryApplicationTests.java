66d4ce261ec74ffd7400bc5db48249e778a3ac53
amarjeet-arora <amarjeet.sn1@gmail.com>
2023-06-23 14:37:23 +0530

    Add files via upload

---
+++ product-discovery/src/test/java/com/example/boades/BoaDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoaDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
